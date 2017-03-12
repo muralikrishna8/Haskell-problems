bed9897d06e216f9f92a50f0f03045f9ad04eca3
Murali Krishna <vmurali1444@gmail.com>
2017-03-12 17:43:45 +0530

    [lists] - finding last but one element

--- lists.hs
+++ lists.hs
@@ -5,3 +5,11 @@
 myLast :: [a] -> a
 myLast [a] = a
 myLast (_:xs) = myLast xs
+
+-- Find the last but on element of a list
+-- myButLast [1,2,3,4] -> 3
+-- myButLast ['x', 'y', 'z'] -> 'y'
+
+myButLast :: [a] -> a
+myButLast (x:[_]) = x
+myButLast (_:xs) = myButLast xs
