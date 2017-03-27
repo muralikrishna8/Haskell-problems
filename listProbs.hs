04566ef15e197bf623cd991b797305d02ecc7454
Murali Krishna <vmurali1444@gmail.com>
2017-03-27 19:45:32 +0530

    [lists] - replicate the elements of a list for a given number of times

--- listProbs.hs
+++ listProbs.hs
@@ -7,3 +7,18 @@ dupli (x:xs) = x:x:dupli xs
 
 dupli' list = concat [[x, x] | x <- list]
 
+-- Repeat a number for given number of times
+repeat' 0 _ = []
+repeat' rep val = val:repeat' (rep-1) val
+
+
+-- Replicate the elements of a list a given number of times
+-- repli :: [a] -> Int -> [a]
+repli [] _ = []
+repli list rep = concat [replicate rep x | x <- list]
+
+repli' [] _ = []
+repli' list rep = concat [repeat' rep x | x <- list]
+    where
+        repeat'' = repeat' 
+
