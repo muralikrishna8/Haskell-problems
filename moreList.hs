bf13e7287807751634752f910225505c71561932
Murali Krishna <vmurali1444@gmail.com>
2017-04-15 21:19:26 +0530

    [Lists] - concatMap

--- moreList.hs
+++ moreList.hs
@@ -2,3 +2,7 @@
 -- concat flattens a list of lists into just a list of elements.
 concat' :: Foldable t => t [a] -> [a]
 concat' = foldr (\acc x -> acc++x) []
+
+-- Doing concatMap is the same as first mapping a function to a list and then concatenating the list with concat
+concatMap' :: (t -> [a]) -> [t] -> [a]
+concatMap' fn = concat . map (\elem -> fn elem)
