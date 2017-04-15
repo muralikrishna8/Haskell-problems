ccee470f885420e5030ebf9db4eeb447baf91746
Murali Krishna <vmurali1444@gmail.com>
2017-04-15 20:55:06 +0530

    [Data.List] - intercalate

--- dataList.hs
+++ dataList.hs
@@ -7,3 +7,9 @@ intersperse elem = init . concat . map (\el -> el : [elem])
 intersperse' :: a -> [a] -> [a]
 intersperse' elem = init . foldr (\x acc -> x:elem:acc) []
 
+
+-- intercalate takes a list of lists and a list. It then inserts that list in between all those lists and then flattens the result.
+-- intercalate " " ["hey","there","guys"] -> "hey there guys"  
+-- intercalate [0,0,0] [[1,2,3],[4,5,6],[7,8,9]] --> [1,2,3,0,0,0,4,5,6,0,0,0,7,8,9]  
+intercalate :: [a] -> [[a]] -> [a]
+intercalate list lists = (foldr (\elem acc -> elem++list++acc) [] $ init lists) ++ last lists
