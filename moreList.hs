1510f5473c8b9c8ee188d74679b7979ab1ecd9a7
Murali Krishna <vmurali1444@gmail.com>
2017-04-15 22:00:16 +0530

    [Lists] - iterate

--- moreList.hs
+++ moreList.hs
@@ -20,3 +20,13 @@ any' fn list = or $ map(\el -> fn el) list
 
 all' :: (t -> Bool) -> [t] -> Bool
 all' fn = and . map(\el -> fn el)
+
+
+-- iterate takes a function and a starting value. It applies the function to the starting value, then it applies that function to the result, then it applies the function to that result again, etc. It returns all the results in the form of an infinite list.
+-- take 10 $ iterate (*2) 1 -> [1,2,4,8,16,32,64,128,256,512]
+-- take 3 $ iterate (++ "haha") "haha" -> ["haha","hahahaha","hahahahahaha"]
+
+iterate' :: (a -> a) -> a -> [a]
+iterate' fn start = iterate'' [start] start
+                    where iterate'' list acc = list ++ let fnVal = fn acc
+                                                       in iterate'' [fnVal] (fnVal)
