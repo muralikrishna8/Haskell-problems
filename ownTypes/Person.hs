3d5e9e6eb0b35456e64901f85948221b57ec8788
Murali Krishna <vmurali1444@gmail.com>
2018-04-24 14:28:42 +0530

    [ownType] - Create a person data type using Record Syntax (named parameters)

---
+++ ownTypes/Person.hs
@@ -0,0 +1,12 @@
+data Person = Person
+ { firstName :: String
+ , lastName :: String
+ , age :: Int
+ , height :: Float
+ , phoneNumber :: String
+ , favoriteFlavor :: String
+ } deriving Show
+
+-- Person {firstName = "Murali", lastName = "Krishna", age = 23, height = 6.2, phoneNumber = "123", favoriteFlavor = "Vanilla"}
+-- firstName Person = Murali
+-- age Person = 23
